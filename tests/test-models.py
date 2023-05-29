51bac1348dced656043b6dc2994d2f5a4c2177e3
Invictus <max.25jr@gmail.com>
2023-05-29 16:26:24 -0400

    Delete test-models.py

--- tests/test-models.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from restaurant.models import Menu
-
-class MenuItemTest(TestCase):
-    def test_get_item(self):
-        item = Menu.objects.create(name="Lemon dessert", price=7.00, menu_item_description="")
-        itemstr = item.get_item()
-        self.assertEqual(itemstr, "Lemon dessert : 17.00")
